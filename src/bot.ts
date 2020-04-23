2c1ac034f42a845ef91c13995dd532f67fbb4afb
Martin Löffler <git@fatalerrorcoded.eu>
2020-04-23 23:00:54 +0200

    Rate my meme feature

--- src/bot.ts
+++ src/bot.ts
@@ -7,23 +7,30 @@ import * as path from "path";
 import Discord from "discord.js";
 
 const client = new Discord.Client();
+const videoPath = path.join(__dirname, "..", "videos");
+const videos: {name: string, data: Buffer}[] = [];
 
-const playVideo = (video: string, channel: Discord.TextBasedChannelFields) => {
-    fs.readFile(path.join(__dirname, "..", "videos", video + ".mp4"), null, (err, videoBuffer) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
+console.log("Loading videos");
+const videoFiles = fs.readdirSync(videoPath);
+for (let videoFile of videoFiles) {
+    const data = fs.readFileSync(path.join(videoPath, videoFile), null);
+    videos.push({ name: path.parse(videoFile).name.toLowerCase(), data });
+    console.log(`Loaded ${videoFile}`);
+}
 
-        const attachment = new Discord.MessageAttachment(videoBuffer, video + ".mp4");
-        channel.send(attachment);
-    });
+const playVideo = (videoName: string, channel: Discord.TextBasedChannelFields, displayName?: string) => {
+    let video = videos.find((video) => video.name == videoName);
+    if (video === undefined) throw new Error(`Video ${videoName}.mp4 not found`);
+
+    let name = displayName || video.name;
+    const attachment = new Discord.MessageAttachment(video.data, name + ".mp4");
+    channel.send(attachment);
 }
 
 client.on("message", (message) => {
     if (message.author.bot) return;
 
-    switch (message.content) {
+    switch (message.content.toLowerCase()) {
         case "meme approved":
             playVideo("approved", message.channel);
             break;
@@ -38,6 +45,10 @@ client.on("message", (message) => {
             break;
         case "meme failed":
             playVideo("failed", message.channel);
+            break;
+        case "rate my meme":
+            let video = videos[Math.floor(Math.random() * videos.length)];
+            playVideo(video.name, message.channel, "rating");
             break;
         default: break;
     }
